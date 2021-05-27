7d54bf1ff1ac8366fa1f05ce54d8235c03e30603
Zedan Saheer <zed.saheer5@gmail.com>
2021-05-27 16:19:55 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -19,7 +19,21 @@ document.querySelector('.menuforresponsive').addEventListener('click',()=>{
     document.querySelector('.nav-mobile').classList.toggle('active');
 })
 
+/* document.querySelector('.menuforresponsive').addEventListener('click',()=>{
+    document.querySelector('.tricont').classList.toggle('active');
+})
+
+document.querySelector('.menuforresponsive').addEventListener('click',()=>{
+    document.querySelector('.aside').classList.toggle('active');
+}) */
+
 const acc = document.querySelectorAll(".accordianbutton");
+
+acc.forEach((acc) => {
+    acc.addEventListener('click',()=>
+        acc.classList.toggle('active'));
+    })
+
 
 acc.forEach((acc) => {
     acc.addEventListener('click',()=>
@@ -27,8 +41,58 @@ acc.forEach((acc) => {
 })
 
 const cart = document.querySelector("aside");
-const toggle = document.querySelector(".sbagtoggle")
+const toggle = document.querySelector(".sbagtoggle");
+const tricart= document.querySelector(".tricont");
+const menu = document.querySelector(".mobile");
+const classesmenu = menu.classList;
+const result = classesmenu.contains("active");
+const answer = cart.classList.remove("toggle");
+
 
 toggle.addEventListener('click',()=>{
-    cart.classList.toggle("toggle")
+    cart.classList.toggle("toggle");
 })
+
+toggle.addEventListener('click',()=>{
+    tricart.classList.toggle("toggle");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* if(result && toggle){
+    cart.style.display="none";
+} else {
+    cart.style.display="none";
+}
+
+if(result && toggle){
+    tricart.style.display="none";
+} else {
+    tricart.style.display="none";
+} */
+
+/* if(cart.classList.contains("toggle")){
+    cart.style.display="none";
+} else {
+    cart.style.display="block"
+}
+
+if(cart.classList.contains("toggle")){
+    tricart.style.display="none";
+} else {
+    tricart.style.display="block";
+} */
+
+
+
